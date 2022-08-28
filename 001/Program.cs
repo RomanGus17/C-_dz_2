3c666008c8d1132a300114f9538a2d56384d9f58
Roman <romangushchintlt@gmail.com>
2022-08-28 17:52:20 +0400

    Выполнил задачи 10 и 13

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Write("Введите трёхзначное число: ");
+// int EnterNumber = Convert.ToInt32(Console.ReadLine());
+// string strNumber = Convert.ToString(EnterNumber);
+// Console.WriteLine("Вторая цифра введенного числа = "+strNumber[1]);
+
+//----------------------------------------------------------------
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.Write("Введите число: ");
+// int EnterNumber = Convert.ToInt32(Console.ReadLine());
+// string strNumber = Convert.ToString(EnterNumber);
+// if (strNumber.Length > 2)
+// {
+//     Console.WriteLine("Третья цифра введенного числа = "+strNumber[2]);
+// } 
+// else     
+//     Console.WriteLine ("третьей цифры нет");
+
+
+//----------------------------------------------------------------
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
